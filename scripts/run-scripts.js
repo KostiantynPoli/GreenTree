2b996e33fda6ab2cfc8f4564dac49a83ca472a2b
Kostiantyn <rjcnzod@gmail.com>
2024-08-01 00:51:01 +0300

    add preloader and fix footer

---
+++ scripts/run-scripts.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//     $(".owl-carousel").owlCarousel();
+// });
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 6500,
+        autoplaySpeed: 1000,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            1100: {
+                items: 4
+            },
+        }
+    })
+})
